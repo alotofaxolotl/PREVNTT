881b4b647b504e41a8a166ab52a7e26500240383
Jeremy <alottaaxolotl@hotmail.com>
2023-01-30 23:45:43 +1100

    Add middleware to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,18 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
