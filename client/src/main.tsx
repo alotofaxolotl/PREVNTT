4ab24c60d951a61458b39c017c0ce5fbfe28bd4f
Jeremy <alottaaxolotl@hotmail.com>
2023-01-30 23:27:03 +1100

    Remove unnecessary files from vite client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,11 @@
+// Modules
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// Components
 import App from './App'
+
+// Styles
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
