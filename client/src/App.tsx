0f3fe70a0a1bb44c73896ff60e234c7984b0d6e4
Jeremy <alottaaxolotl@hotmail.com>
2023-01-30 23:34:46 +1100

    Add Tailwind to client * Create landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,19 @@
 export default function App() {
-  return <></>;
+  return (
+    <div className="text-center">
+      <h1 className="text-[5em] font-bold mt-12 mb-4 text-slate-800 drop-shadow-lg">
+        Hello, PREVNTT!
+      </h1>
+      <p className="text-xl">
+        To get started with the PREVNTT stack, check out {""}
+        <a
+          className="underline cursor-pointer text-blue-600"
+          href="https://github.com/alotofaxolotl/PREVNTT"
+          target="_blank"
+        >
+          the GitHub repository.
+        </a>
+      </p>
+    </div>
+  );
 }
